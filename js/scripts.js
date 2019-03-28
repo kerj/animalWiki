3619771d3d2458d94279ddffcbb7353dd5c61209
Heather Zurek and Justin Kerntz <heather@>
2019-03-28 10:42:34 -0700

    add front-end jquery, html p and h4 and css

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,17 @@ $(document).ready(function()  {
   $("form").submit(function(event) {
     var animal = parseInt($("#animalSelect").val());
     if (animal === 1) {
-      $()
+      $("#turtle").removeClass("hidden");
+      $("#snake").addClass("hidden");
+      $("#gator").addClass("hidden");
     }else if (animal === 2) {
-
+      $("#snake").removeClass("hidden");
+      $("#turtle").addClass("hidden");
+      $("#gator").addClass("hidden");
     }else {
-
+      $("#gator").removeClass("hidden");
+      $("#snake").addClass("hidden");
+      $("#turtle").addClass("hidden");
     }
     event.preventDefault();
   });
